6d5b11dc0a5958b6917ac705a74c705b0882a53d
Rogier Spieker <rogier@konfirm.io>
2022-02-04 08:54:33 +0100

    Added documentation blocks

--- source/Domain/Iterator/IterablePair.ts
+++ source/Domain/Iterator/IterablePair.ts
@@ -1,10 +1,28 @@
+/**
+ * Iterator class which traverses all value pairs of two or more Iterable instances
+ *
+ * @export
+ * @class IterablePairIterator
+ * @template T
+ */
 export class IterablePairIterator<T = unknown> {
     private readonly iterators: Array<Iterable<T>> = [];
 
-    constructor(...iterators: Array<Iterable<T>>) {
+    /**
+     * Constructs a new instance of IterablePairIterator
+     * 
+     * @param iterators 
+     */
+    constructor(...iterators: [Iterable<T>, Iterable<T>, ...Array<Iterable<T>>]) {
         this.iterators = iterators;
     }
 
+    /**
+     * Generator yielding all value pairs from the provided iterators
+     *
+     * @return {*}  {Iterator<[T, T]>}
+     * @memberof IterablePairIterator
+     */
     *[Symbol.iterator](): Iterator<[T, T]> {
         for (const [a, b] of this.pairs(this.iterators)) {
             for (const one of a) {
@@ -15,8 +33,15 @@ export class IterablePairIterator<T = unknown> {
         }
     }
 
+    /**
+     * Create unique Iterator<T> combinations to traverse over
+     *
+     * @private
+     * @param {Array<Iterable<T>>} source
+     * @return {*}  {Array<[Iterable<T>, Iterable<T>]>}
+     * @memberof IterablePairIterator
+     */
     private pairs(source: Array<Iterable<T>>): Array<[Iterable<T>, Iterable<T>]> {
         return source.map((a, i) => source.slice(i + 1).map((b) => [a, b])).flat() as Array<[Iterable<T>, Iterable<T>]>;
-        // return source.reduce((carry, a) => carry.concat(source.filter((b) => a !== b).map((b) => [a, b])), [] as Array<[Iterable<T>, Iterable<T>]>);
     }
 }
--- source/Domain/Iterator/SimpleGeometry.ts
+++ source/Domain/Iterator/SimpleGeometry.ts
@@ -6,13 +6,31 @@ import { GeometryCollection } from "../GeoJSON/GeometryCollection";
 
 type SimpleGeometry = Point | LineString | Polygon;
 
+/**
+ * Iterator class to turn any GeoJSON structure into one or more SimpleGeometry (Point | LineString | Polygon) objects
+ *
+ * @export
+ * @class SimpleGeometryIterator
+ */
 export class SimpleGeometryIterator {
     private readonly inputs: Array<GeoJSON> = [];
 
-    constructor(...inputs: Array<GeoJSON>) {
+    /**
+     *  Creates an instance of SimpleGeometryIterator
+     * 
+     * @param {...[GeoJSON, ...Array<GeoJSON>]} inputs
+     * @memberof SimpleGeometryIterator
+     */
+    constructor(...inputs: [GeoJSON, ...Array<GeoJSON>]) {
         this.inputs = inputs;
     }
 
+    /**
+     * Generator yielding all SimpleGeometry objects from the provided GeoJSON structure(s)
+     *
+     * @return {*}  {Iterator<SimpleGeometry>}
+     * @memberof SimpleGeometryIterator
+     */
     *[Symbol.iterator](): Iterator<SimpleGeometry> {
         for (const input of this.inputs) {
             for (const simple of this.unwrap(input)) {
@@ -21,40 +39,96 @@ export class SimpleGeometryIterator {
         }
     }
 
+    /**
+     * unwrap any GeoJSON object into one or more SimpleGeometry object
+     *
+     * @private
+     * @param {GeoJSON} geo
+     * @return {*}  {Iterable<SimpleGeometry>}
+     * @memberof SimpleGeometryIterator
+     */
     private *unwrap(geo: GeoJSON): Iterable<SimpleGeometry> {
         geo.type in this
             ? yield* this[geo.type](geo)
             : yield geo as SimpleGeometry;
     }
 
+    /**
+     * Generate the geometries in a GeometryCollection
+     *
+     * @private
+     * @param {GeometryCollection} { geometries }
+     * @return {*}  {Iterable<SimpleGeometry>}
+     * @memberof SimpleGeometryIterator
+     */
     private *GeometryCollection({ geometries }: GeometryCollection): Iterable<SimpleGeometry> {
         for (const geometry of geometries) {
             yield* this.unwrap(geometry);
         }
     }
 
+    /**
+     * Generate the geometries of the Feature object(s) in a FeatureCollection
+     *
+     * @private
+     * @param {FeatureCollection} { features }
+     * @return {*}  {Iterable<SimpleGeometry>}
+     * @memberof SimpleGeometryIterator
+     */
     private *FeatureCollection({ features }: FeatureCollection): Iterable<SimpleGeometry> {
         for (const { geometry } of features) {
             yield* this.unwrap(geometry);
         }
     }
 
+    /**
+     * Generate the geometry of a Feature object
+     *
+     * @private
+     * @param {Feature} { geometry }
+     * @return {*}  {Iterable<SimpleGeometry>}
+     * @memberof SimpleGeometryIterator
+     */
     private *Feature({ geometry }: Feature): Iterable<SimpleGeometry> {
         yield* this.unwrap(geometry);
     }
 
+    /**
+     * Generate Point objects from a MultiPoint object
+     *
+     * @private
+     * @param {MultiPoint} multi
+     * @return {*}  {Iterable<Point>}
+     * @memberof SimpleGeometryIterator
+     */
     private *MultiPoint(multi: MultiPoint): Iterable<Point> {
         for (const coordinates of multi.coordinates) {
             yield { type: 'Point', coordinates };
         }
     }
 
+    /**
+     * Generate LineString objects from a MultiLineString object
+     *
+     * @private
+     * @param {MultiLineString} multi
+     * @return {*}  {Iterable<LineString>}
+     * @memberof SimpleGeometryIterator
+     */
     private *MultiLineString(multi: MultiLineString): Iterable<LineString> {
         for (const coordinates of multi.coordinates) {
             yield { type: 'LineString', coordinates };
         }
     }
 
+    /**
+     * Generate Polygon objects from a MultiPolygon object
+     *
+     * @private
+     * @param {MultiPolygon} multi
+     * @return {*}  {Iterable<Polygon>}
+     * @memberof SimpleGeometryIterator
+     */
     private *MultiPolygon(multi: MultiPolygon): Iterable<Polygon> {
         for (const coordinates of multi.coordinates) {
             yield { type: 'Polygon', coordinates };
