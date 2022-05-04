573d2d1bb61cbafe940efea0767bc1c0b9ca8836
Rogier Spieker <rogier@konfirm.io>
2022-05-04 23:57:57 +0200

    removed coordinates-only checks from public exports

--- source/main.ts
+++ source/main.ts
@@ -1,15 +1,15 @@
-export * from './Domain/GeoJSON/GeoJSON';
-export * from './Domain/GeoJSON/Geometry/Point';
-export * from './Domain/GeoJSON/Geometry/MultiPoint';
-export * from './Domain/GeoJSON/Geometry/LineString';
-export * from './Domain/GeoJSON/Geometry/MultiLineString';
-export * from './Domain/GeoJSON/Geometry/Polygon';
-export * from './Domain/GeoJSON/Geometry/MultiPolygon';
-export * from './Domain/GeoJSON/GeometryCollection';
-export * from './Domain/GeoJSON/Feature';
-export * from './Domain/GeoJSON/FeatureCollection';
-
 export { intersect } from './Domain/Utility/Intersect';
 export { distance } from './Domain/Utility/Distance';
 
 export { SimpleGeometryIterator } from './Domain/Iterator/SimpleGeometry';
+
+export { GeoJSON, isGeoJSON, isStrictGeoJSON } from './Domain/GeoJSON/GeoJSON';
+export { Point, isPoint, isStrictPoint } from './Domain/GeoJSON/Geometry/Point';
+export { MultiPoint, isMultiPoint, isStrictMultiPoint } from './Domain/GeoJSON/Geometry/MultiPoint';
+export { LineString, isLineString, isStrictLineString } from './Domain/GeoJSON/Geometry/LineString';
+export { MultiLineString, isMultiLineString, isStrictMultiLineString } from './Domain/GeoJSON/Geometry/MultiLineString';
+export { Polygon, isPolygon, isStrictPolygon } from './Domain/GeoJSON/Geometry/Polygon';
+export { MultiPolygon, isMultiPolygon, isStrictMultiPolygon } from './Domain/GeoJSON/Geometry/MultiPolygon';
+export { GeometryCollection, isGeometryCollection, isStrictGeometryCollection } from './Domain/GeoJSON/GeometryCollection';
+export { Feature, isFeature, isStrictFeature } from './Domain/GeoJSON/Feature';
+export { FeatureCollection, isFeatureCollection, isStrictFeatureCollection } from './Domain/GeoJSON/FeatureCollection';
